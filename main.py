eba0bec1d7ebfa8930298d19e8b42d5035d7daae
Teemust407 <92663125+Teemust407@users.noreply.github.com>
2025-03-10 12:01:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, this is your LINE Bot!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return "Webhook received!", 200  # ✅ ต้องคืนค่า 200
 
 if __name__ == "__main__":
     app.run()
